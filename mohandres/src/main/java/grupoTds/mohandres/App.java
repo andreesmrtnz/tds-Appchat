0069f5ae7cf7f115dd263220bdd71aedbb51816f
mohaxx7 <mohamed.b.z@um.es>
2024-10-15 09:36:14 +0200

    prueba de git

---
+++ mohandres/src/main/java/grupoTds/mohandres/App.java
@@ -0,0 +1,13 @@
+package grupoTds.mohandres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
