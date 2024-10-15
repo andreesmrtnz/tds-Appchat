20c98931053397ebda32776359d6714447d4f36a
mohaxx7 <mohamed.b.z@um.es>
2024-10-15 09:44:23 +0200

    pruebas

--- g2/src/main/java/umu/tds/g2/App.java
+++ g2/src/main/java/umu/tds/g2/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello!" );
-        System.out.println("world");
+        System.out.println( "andres " );
+        System.out.println(" adios ");
         
     }
 }
